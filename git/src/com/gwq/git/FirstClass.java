7b6a3719f01d927d37cbadc31b8557af0a6eb9f3
admin <353206517@qq.com>
2019-03-21 21:05:34 +0800

    修改FirstClass

--- git/src/com/gwq/git/FirstClass.java
+++ git/src/com/gwq/git/FirstClass.java
@@ -5,13 +5,14 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("大佬来了");
-
+//       hellotest
     }
 
 
     public void test(){
         int i=0;
         i+=10;
+        System.out.println(i);
     }
 
 }
